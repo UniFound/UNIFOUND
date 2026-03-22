1f0654c2668cc91f43bde267493bd0296347b17f
U L R Kavindya <it23600898@my.sliit.lk>
2026-03-22 18:18:49 +0530

    Install Dependencies 😁

--- UniFound-Frontend/src/App.jsx
+++ UniFound-Frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import LoginPage from './pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <LoginPage />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
